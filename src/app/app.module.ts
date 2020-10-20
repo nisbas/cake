8b215cc1c7c1be96a4adffe6fe2f7453960b437b
Nishan <nishu3691@gmail.com>
2020-10-20 08:54:33 -0500

    adding cakeJs files

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { TopNavbarComponent } from './Components/top-navbar/top-navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    TopNavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
